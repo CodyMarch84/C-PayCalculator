95db9c59306a588e6fdee403ea9c09ebe444f2b7
Cody March <cody.march84@gmail.com>
2017-12-19 12:00:43 -0600

    added more functions

--- PayCalculator/PayCalculator.cs
+++ PayCalculator/PayCalculator.cs
@@ -6,38 +6,87 @@ using System.Threading.Tasks;
 
 namespace PayCalculator
 {
+    /*private float CalculatePay()
+    {
+        string hoursWorked;
+        string payRate;
+        float standardWeek = 40;
+        float overTimeRate = 1.5f;
+        float paycheck = 0;
+
+        Console.WriteLine("Enter hours worked this week: ");
+            hoursWorked = Console.ReadLine();
+        Console.WriteLine("Enter hourly rate: ");
+            payRate = Console.ReadLine();
+        float hours = float.Parse(hoursWorked);
+        float rate = float.Parse(payRate);
+
+        if (hours > standardWeek)
+        {
+            float overTimeHours = hours - standardWeek;
+            float regPay = standardWeek * rate;
+            float overTimePay = rate * overTimeRate * overTimeHours;
+            paycheck = overTimePay + regPay;
+        }
+        else
+        {
+            paycheck = hours * rate;
+        }
+        Console.WriteLine("Your pay check is {0}.", paycheck);
+        Console.ReadKey();
+        return paycheck;
+
+    }*/
     public class PayCalculator
     {
-        private float CalculatePay()
+        private string hoursWorked;
+        private string payRate;
+        private float paycheck;
+
+        private void CalculatePay()
         { 
-            string hoursWorked;
-            string payRate;
+            
+        }
+
+        public string GetUserHours()
+        {
+            Console.WriteLine("Enter hours worked this week: ");
+                hoursWorked = Console.ReadLine();
+            return hoursWorked;
+        }
+
+        public string GetUserRate()
+        {
+            Console.WriteLine("Enter hourly rate: ");
+            payRate = Console.ReadLine();
+            return payRate;
+        }
+
+        public float GetOverTimeHours(float hours, float rate)
+        {
             float standardWeek = 40;
             float overTimeRate = 1.5f;
             float paycheck = 0;
 
-            Console.WriteLine("Enter hours worked this week: ");
-                hoursWorked = Console.ReadLine();
-            Console.WriteLine("Enter hourly rate: ");
-                payRate = Console.ReadLine();
-            float hours = float.Parse(hoursWorked);
-            float rate = float.Parse(payRate);
-
-            if(hours > standardWeek)
-                {
+            if (hours > standardWeek)
+            {
                 float overTimeHours = hours - standardWeek;
                 float regPay = standardWeek * rate;
                 float overTimePay = rate * overTimeRate * overTimeHours;
-                paycheck = overTimePay + regPay; 
-                }
+                paycheck = overTimePay + regPay;
+            }
             else
-                {
+            {
                 paycheck = hours * rate;
-                }
+            }
+            return paycheck;
+        }
+
+        public float DisplayPay()
+        {
             Console.WriteLine("Your pay check is {0}.", paycheck);
             Console.ReadKey();
             return paycheck;
-            
         }
      }
 }
